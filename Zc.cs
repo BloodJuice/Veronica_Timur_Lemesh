35aa18aede54cd597da623753135b65953b23a06
BloodJuice <falcon.with.hat@gmail.com>
2023-09-03 22:08:38 +0300

    Maybe works

--- Zc.cs
+++ Zc.cs
@@ -7,34 +7,6 @@ class Zc
     private int k;
     
     public Zc(List<int> ni) { this.ni = ni; k = ni.Count(); }
-
-    public List<Double> NormalGenerateX(int count)
-    {
-        Random rand = new Random();
-        List<Double> res = new List<double>();
-
-        for (int i = 0; i < ni[count]; i++)
-        {
-            double u1 = rand.NextDouble();
-            double u2 = rand.NextDouble();
-            if (i % 2 == 0) {
-                res.Add(Math.Sqrt(-2.0 * Math.Log(u1)) * Math.Sin(2.0 * Math.PI * u2));
-            }
-            if (i % 2 != 0)
-            {
-                res.Add(Math.Sqrt(-2.0 * Math.Log(u1)) * Math.Cos(2.0 * Math.PI * u2));
-            }
-        }
-        CocktailSort(res);
-        return res;
-    }
-
-    //public List<Double> MainFunction()
-    //{
-    //    int n = 0;
-    //    for (int i = 0; i < ni.Count(); i++) { n += ni[i]; }
-
-    //}
     private void Swap(List<Double> array, int i, int j)
     {
         Double temp = array[i];
@@ -64,6 +36,49 @@ class Zc
             left++;
         }
     }
+
+    public List<Double> NormalGenerateX(int count)
+    {
+        Random rand = new Random();
+        List<Double> res = new List<double>();
+
+        for (int i = 0; i < ni[count]; i++)
+        {
+            double u1 = rand.NextDouble();
+            double u2 = rand.NextDouble();
+            if (i % 2 == 0) {
+                res.Add(Math.Sqrt(-2.0 * Math.Log(u1)) * Math.Sin(2.0 * Math.PI * u2));
+            }
+            if (i % 2 != 0)
+            {
+                res.Add(Math.Sqrt(-2.0 * Math.Log(u1)) * Math.Cos(2.0 * Math.PI * u2));
+            }
+        }
+        CocktailSort(res);
+        return res;
+    }
+
+    public List<Double> MainFunction(int N)
+    {
+        int n = 0;
+        
+    }
+    private double FunctionZc(int n)
+    {
+        double result = 0.0;
+        for (int i = 0; i < ni.Count(); i++) { n += ni[i]; }
+        for (int i = 0; i < k; i++)
+        {
+            double Rij = 1.0;
+            for (int j = 0; j < k; j++, Rij++)
+            {
+                result += Math.Log(ni[i] / ((j + 1.0) - 0.5) - 1.0) * Math.Log(n / ((Rij + 1.0) - 0.5) - 1.0);
+            }
+        }
+        result *= 1.0 / n;
+        return result;
+    }
+
 
 }
 
