f4323a93909429b83ad1f38519a9dc171f783d5f
BloodJuice <falcon.with.hat@gmail.com>
2023-09-03 21:33:58 +0300

    Normal distr is working

--- Program.cs
+++ Program.cs
@@ -5,7 +5,12 @@ namespace Program
 {
     internal class Program
     {
-
+        static void Main(string[] args)
+        {
+            List<int> ni = new List<int> { 20, 20 }; 
+            Zc value = new Zc(ni);
+            value.NormalGenerateX(0);
+        }
     }
 }
 
--- Zc.cs
+++ Zc.cs
@@ -3,12 +3,12 @@ using System;
 
 class Zc
 {
-    private List<Double> ni;
+    private List<int> ni;
     private int k;
     
-    public Zc(List<Double> ni, int k) { this.ni = ni; this.k = k; }
+    public Zc(List<int> ni) { this.ni = ni; k = ni.Count(); }
 
-    private List<Double> NormalGenerateX(List<Double> ni, int count)
+    public List<Double> NormalGenerateX(int count)
     {
         Random rand = new Random();
         List<Double> res = new List<double>();
@@ -25,7 +25,44 @@ class Zc
                 res.Add(Math.Sqrt(-2.0 * Math.Log(u1)) * Math.Cos(2.0 * Math.PI * u2));
             }
         }
+        CocktailSort(res);
         return res;
+    }
+
+    //public List<Double> MainFunction()
+    //{
+    //    int n = 0;
+    //    for (int i = 0; i < ni.Count(); i++) { n += ni[i]; }
+
+    //}
+    private void Swap(List<Double> array, int i, int j)
+    {
+        Double temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    private void CocktailSort(List<Double> inArray)
+    {
+        int left = 0,
+             right = inArray.Count - 1;
+
+        while (left < right)
+        {
+            for (int i = left; i < right; i++)
+            {
+                if (inArray[i] > inArray[i + 1])
+                    Swap(inArray, i, i + 1);
+            }
+            right--;
+
+            for (int i = right; i > left; i--)
+            {
+                if (inArray[i - 1] > inArray[i])
+                    Swap(inArray, i - 1, i);
+            }
+            left++;
+        }
     }
 
 }
